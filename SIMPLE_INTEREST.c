953d3e7bcc22ac3703aa018bb3179346df8eff55
Doddalavanyaumabhavani <119916455+lavanyauma@users.noreply.github.com>
2023-05-02 11:07:45 +0530

    #include<stdio.h> void ss(int p,int t,int r); int main() {     int p,t,r;     scanf("%d%d%d",&p,&t,&r);     ss(p,t,r); } void ss(int p,int t,int r) {     int s;     s=(p*t*r)/100;     {       printf("%d",s);     } }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-int ss(int p,int t,int r);
+void ss(int p,int t,int r);
 int main()
 {
     int p,t,r;
     scanf("%d%d%d",&p,&t,&r);
-    int x=ss(p,t,r);
+    ss(p,t,r);
 }
-int ss(int p,int t,int r)
+void ss(int p,int t,int r)
 {
     int s;
     s=(p*t*r)/100;
     {
       printf("%d",s);
     }
-    return 0;
 }
